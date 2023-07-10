de2a3b4c62ff092a936b16227039246572bd88d8
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-07-09 21:10:43 -0300

    :sparkles: feat: Componente ClaimUsernameForm criado
    Componente onde a pessoa irá reservar seu usuário estruturado e estilizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './styles'
 import Image from 'next/image';
 import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
                 Agendamento descomplicado
             </Heading>
             <Text size="xl"> Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre. </Text>
+            <ClaimUsernameForm />
         </Hero>
         <Preview>
             <Image
