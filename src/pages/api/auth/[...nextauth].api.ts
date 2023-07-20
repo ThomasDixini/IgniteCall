5867ec40cd6fdc2b8da1d860e5c9e65459d9fbfa
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-07-19 21:34:35 -0300

    :sparkles: feat: Permissões do usuário
    Criando algumas autorizações de escopo, para que o usuário nos permita ter acesso a alguns recursos de sua conta google. E também uma callback para caso ele não conceda, um parâmetro de erro seja retornado

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,8 +6,23 @@ export const authOptions: AuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? '',
             clientSecret: process.env.GOOGLE_SECRET_ID ?? '',
+            authorization: {
+                params: {
+                    scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar'
+                }
+            }
         })
-    ]
+    ],
+
+    callbacks: {
+        async signIn({ account }) {
+            if (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
+                return 'https://localhost:3000/register/connect-calendar/?error=permissions'
+            }
+
+            return true;
+        }
+    }
 }
 
 export default NextAuth(authOptions)
