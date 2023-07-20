13f9639f587a574261a79f15a40018d07930a74b
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-07-19 21:10:37 -0300

    :sparkles: feat: Autenticação com oAuth
    Criei uma api route /auth que utiliza o Next Auth para autenticação com a conta google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -2,6 +2,7 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function Register() {
     /*async function handleRegister(){
@@ -24,7 +25,7 @@ export default function Register() {
                     <Text>
                         Google Calendar
                     </Text>
-                    <Button variant="secondary" size="sm">
+                    <Button variant="secondary" size="sm" onClick={() => signIn('google')}>
                         Conectar
                         <ArrowRight />
                     </Button>
